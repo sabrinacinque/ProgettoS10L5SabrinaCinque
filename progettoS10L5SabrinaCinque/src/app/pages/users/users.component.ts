33f9fb478dfb111834dcd90f3da4289305259797
sabrinacinque <cinque.sabrina@gmail.com>
2024-05-31 13:18:40 +0200

    terzocommit

--- progettoS10L5SabrinaCinque/src/app/pages/users/users.component.ts
+++ progettoS10L5SabrinaCinque/src/app/pages/users/users.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { iTodo } from '../../Models/i-todo';
+import { iUser } from '../../Models/i-user';
+import { TodoService } from '../../todo.service';
+import { UserService } from '../../user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  usersWithTodos: (iUser & { todos?: iTodo[] })[] = [];
 
+  constructor(private svcUser: UserService, private todoService: TodoService) { }
+
+  ngOnInit() {
+    const todos = this.todoService.getTodos();
+    this.usersWithTodos = this.svcUser.getUsersWithTodos(todos);
+  }
 }
